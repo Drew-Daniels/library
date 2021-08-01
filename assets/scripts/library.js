66de990624906dad1cdf2971344491bf6a88c031
DrewDaniels <drew@thedaniels.com>
2021-08-01 11:04:05 -0500

    Created a basic working version of a library where you can now add new entries. Will be working on having the library not duplicate book entries when a new book is added.

--- assets/scripts/library.js
+++ assets/scripts/library.js
@@ -16,8 +16,41 @@ function Book(title, author, pageNumber, readStatus=false) {
 	}
 }
 
-function getBookInfo() {
-    FormData()
+const popup = document.querySelector('#myPopup');
+const bookEntry = document.querySelector('#bookTitle');
+const authorEntry = document.querySelector('#authorName');
+const pageEntry = document.querySelector('#numPages');
+const readEntry = document.querySelector('#readStatus');
+
+function openForm() {
+    popup.classList.add('show');
+}
+
+function submitForm() {
+    let book;
+    let author;
+    let page;
+    let read; 
+    let newAddition;
+    function getFormData() {
+        book = bookEntry.value;
+        author = authorEntry.value;
+        page = pageEntry.value;
+        read = readEntry.value;
+        newAddition = [book, author, page, read];
+        return newAddition;
+    }
+
+    function closeForm() {
+        popup.classList.remove('show');
+    }
+
+    let entry = getFormData();
+    let newBook = new Book(...entry);
+    addBookToLibrary(newBook);
+    closeForm();
+
+    refreshLibrary();
 }
 
 function addBookToLibrary(bk) {
@@ -27,30 +60,34 @@ function addBookToLibrary(bk) {
 function selfTest() {
     const book1 = new Book('The Great Gatsby', 
                          'F. Scott Fitzgerald',
-                         400,
+                         240,
                          true);
     const book2 = new Book('One Hundred Years of Solitude',
                          'Gabriel Garcia Marquez',
-                         1000,
+                         432,
                          false);
     const book3 = new Book('In Cold Blood',
                          'Truman Capote',
-                         250,
+                         320,
                          false);
     const collection = [book1, book2, book3];
 
     collection.forEach(addBookToLibrary);
 }
 
-selfTest();
-
 const bookList = document.querySelector('#bookTable');
-myLibrary.forEach(function(bk) {
-    let entry = document.createElement('tr');
-    for (let prop in bk) {
-        let fact = document.createElement('td');
-        fact.innerHTML = bk[prop];
-        entry.appendChild(fact);
-    }
-    bookList.appendChild(entry);
-})
+selfTest();
+refreshLibrary();
+
+function refreshLibrary() {
+    myLibrary.forEach(function(bk) {
+        let entry = document.createElement('tr');
+        for (let prop in bk) {
+            let fact = document.createElement('td');
+            fact.innerHTML = bk[prop];
+            entry.appendChild(fact);
+        }
+        bookList.appendChild(entry);
+    })
+}
+
